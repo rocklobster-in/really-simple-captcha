d4d4afcb8a953006a3db0cfec870427594194595
Takayuki Miyoshi <takayukister@gmail.com>
2025-01-28 17:36:47 +0900

    Use WP_Filesystem_Direct if no FS available
    #22

--- includes/filesystem.php
+++ includes/filesystem.php
@@ -7,9 +7,9 @@ trait RSC_Filesystem {
 	public function connect() {
 		global $wp_filesystem;
 
-		if ( ! function_exists( 'WP_Filesystem' ) ) {
-			require_once ABSPATH . '/wp-admin/includes/file.php';
-		}
+		require_once ABSPATH . 'wp-admin/includes/file.php';
+		require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-base.php';
+		require_once ABSPATH . 'wp-admin/includes/class-wp-filesystem-direct.php';
 
 		ob_start();
 		$credentials = request_filesystem_credentials( '' );
@@ -17,10 +17,11 @@ trait RSC_Filesystem {
 
 		if ( false === $credentials or ! WP_Filesystem( $credentials ) ) {
 			wp_trigger_error( __FUNCTION__, __( 'Could not access filesystem.' ) );
-			return;
-		}
 
-		$this->filesystem = $wp_filesystem;
+			$this->filesystem = new WP_Filesystem_Direct( array() );
+		} else {
+			$this->filesystem = $wp_filesystem;
+		}
 	}
 
 }
